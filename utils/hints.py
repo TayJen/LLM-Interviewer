88600b3d820d8d1ec5719d0552bde78027ee1512
eugene killevsky <helloevgenyy@gmail.com>
2024-10-28 10:29:53 +0300

    update hints

--- utils/hints.py
+++ utils/hints.py
@@ -1,25 +1,15 @@
 def get_hint(question: str, topic_idx: int = 0, question_idx: int = 0) -> str:
     topic_hints = {
-        1: {
-            1: "Hint for Topic 1, Question 1",
-            2: "Hint for Topic 1, Question 2",
-        },
-        2: {
-            1: "Hint for Topic 2, Question 1",
-            2: "Hint for Topic 2, Question 2",
-        }
+        (1, 1): "Hint for Topic 1, Question 1",
+        (1, 2): "Hint for Topic 1, Question 2",
+        (2, 1): "Hint for Topic 2, Question 1",
+        (2, 2): "Hint for Topic 2, Question 2",
     }
 
-    question_hints = topic_hints.get(topic_idx, None)
+    question_hints = topic_hints.get((topic_idx, question_idx), None)
     if question_hints is None and question == "":
-        return f"No hint available in topic #{topic_idx+1}."
+        return f"No hint available for question #{question_idx+1} in topic #{topic_idx+1}."
     elif question_hints is None and question:
         return f"Hint for question: ```{question.split(' ')[-1]}```"
 
-    hints = question_hints.get(question_idx, None)
-    if hints is None and question == "":
-        return f"No hint available for question #{question_idx+1} in topic #{topic_idx+1}."
-    elif hints is None and question:
-        return f"Hint for question: ```{question.split(' ')[-1]}```"
-
-    return hints
+    return question_hints
